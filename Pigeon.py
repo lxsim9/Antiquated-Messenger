cda4788ff6b518f7fc7c226816d76ee7e73ee823
falsaif18 <34071749+falsaif18@users.noreply.github.com>
2017-12-07 13:53:34 -0500

    pigeon

---
+++ Pigeon.py
@@ -0,0 +1,19 @@
+class pigeon:
+	def __init__(self, name, body)
+		self.name = "Chyna"
+		self.body = '                    --                                                             
+                       \                 /   @ )
+                         \              _/_ |~ \)
+						   \       ( ( (    \ \
+						   	 ( ( ( ( ( (     | \
+					_ _=(_(_(_(_(_(_(_(_  _ _ / )
+						              - _ _ _  /
+						                   _\__
+						                  `   "' "
+
+	def send_message(self, message)
+		self.message = str(input('What would you like to send? '))
+		print(self.message) 
+
+pigeon()
+		
