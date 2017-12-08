5ff22c8def08f47e536dc110dee9b621d6ef9847
falsaif18 <34071749+falsaif18@users.noreply.github.com>
2017-12-07 20:56:46 -0500

    update pigeon

--- Pigeon.py
+++ Pigeon.py
@@ -1,34 +1,26 @@
 class pigeon:
-	def __init__(self, name, body)
-		self.name = "Chyna"
-		self.body = """
-			   .-''-.
-			  / ,    \
-		       .-'`(o)    ;
-		      '-==.       |
-			   `._...-;-.
-			    )--"""   `-.
-			   /   .        `-.
-			  /   /      `.    `-.
-			  |   \    ;   \      `-._________
-			  |    \    `.`.;          -------`.
-			   \    `-.   \\\\          `---...|
-			    `.     `-. ```\.--'._   `---...|
-			      `-.....7`-.))\     `-._`-.. /
-				`._\ /   `-`         `-.,'
-				  / /
-				 /=(_
-			      -./--' `
-			    ,^-(_
-			    ,--' ` """  
+	def __init__(self, name, body):
+		self.name = name
+		self.body = body 
+		print(self.name)	 
 		print(self.body) 
 	
-			     
-
-
-	def send_message(self, message)
+	def send_message(self, message):
 		self.message = message 
-		return self.message 
-
-
+		print(self.message)
 		
+mypigeon = pigeon("Chyna", """  
+	______ __ 
+	{-_-_= '. `'.
+	{=_=_-  \   \
+	 {_-_   |   /
+	  '-.   |  /    .===,
+	.--.__\  |_(_,==`  ( o)'-.
+	`---.=_ `     ;      `/    \
+	  `,-_       ;    .'--') /
+	    {=_       ;=~`    `"`
+	     `//__,-=~`
+	     <<__ \\__
+	      /`)))/`)))
+""")
+mypigeon.send_message("hey")
