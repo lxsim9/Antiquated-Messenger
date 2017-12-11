895415a32b3d263193b6cbfcf5ec7c1f0792058b
falsaif18 <34071749+falsaif18@users.noreply.github.com>
2017-12-10 21:01:51 -0500

    main
    tried to update this to add the town crier if it still works

--- MainCode.py
+++ MainCode.py
@@ -1,5 +1,5 @@
 while true:
-    ans = str(input("What way would you like to communicate? (Pigeon, Morse, Encrypt)"))
+    ans = str(input("What way would you like to communicate? (Pigeon, Morse, Encrypt, town crier)"))
     if ans.title == "Pigeon":
         a = "p"
         break
@@ -8,6 +8,9 @@ while true:
         break
     elif ans.title == "Encrypt":
         a = "e"
+        break
+    elif ans.title == "town crier":
+        a = "t"
         break
     else:
         print("Sorry, what does "+ans+" mean?")
@@ -18,6 +21,8 @@ if a == "p":
     msg = Pigeon(ms)
 elif a == "m":
     msg = Morse(ms)
+elif a == "t":
+    msg = towncrier(ms)
 else:
     msg = Code(ms)
     
